6e0e93a47f606c5cef0ef654b61563f057de95c8
David Herrera <david@alley.com>
2022-12-24 13:39:38 -0500

    Update Alley contact information

--- tests/alley/wp/test-caper.php
+++ tests/alley/wp/test-caper.php
@@ -2,7 +2,7 @@
 /**
  * Class file for Test_Caper
  *
- * (c) Alley <info@alley.co>
+ * (c) Alley <info@alley.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
